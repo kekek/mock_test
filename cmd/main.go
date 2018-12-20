7884ca2437e1c06f13c2cedac4c6fcbc11eb378d
jinyf <jyf0231@163.com>
2018-12-21 00:19:52 +0800

    add

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/sgoertzen/html2text"
+)
+
+var originContent = `
+	<html >
+		<head> test </head>
+		<body>
+			啦啦啦啦啦啦啦啦啦啦
+		</body>
+	</html>
+`
+
+func main() {
+	fmt.Println("test")
+}
